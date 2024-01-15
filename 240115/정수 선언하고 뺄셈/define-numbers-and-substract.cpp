946dc5e4310aaf98412e76858bc4b30555348b0b
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-15 10:04:55 +0900

    Title: 정수 선언하고 뺄셈, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 97;
+    int b = 13;
+    int sub = a-b;
+
+    cout << a << " - " << b << " = " << sub;
+    return 0;
+}
