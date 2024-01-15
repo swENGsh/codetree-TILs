cd95bcf1f371f2cd285b8cf90e1786a64705781f
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-16 08:48:03 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240116/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 1;
+    int b = 2;
+    char c = 'C';
+
+    cout << a << "->" << b << "->" << c;
+    return 0;
+}
