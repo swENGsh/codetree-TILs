244eb4135f68ce6977722ad9cc3c70971431febd
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-17 09:22:39 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240117/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 3, b = 5;
+    int tmp = a;
+    a = b, b = tmp;
+
+    cout << a << endl;
+    cout << b << endl;
+    
+    return 0;
+}
