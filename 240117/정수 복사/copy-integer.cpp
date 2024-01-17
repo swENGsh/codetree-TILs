880cad34b98a5b8d88c0914c09ff2356056a049c
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-17 09:20:42 +0900

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240117/정수 복사/copy-integer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 3, b = 4;
+    b = a;
+    int ans = a*b;
+
+    cout << a << " " << b << endl;
+    cout << ans;
+    return 0;
+}
