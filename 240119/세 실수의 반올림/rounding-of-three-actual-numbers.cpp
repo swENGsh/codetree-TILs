15de0d736c103590e9750415f925c13a76894121
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-19 11:37:27 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout << fixed;
+    cout.precision(3);
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    return 0;
+}
