06082816194a36e616a745fbcff4f41ba3165bf0
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-19 15:21:23 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    char d;
+    cin >> a >> d >> b >> d >> c >> d;
+    cout << '0' << a << d << c << d << b;
+    return 0;
+}
