98126a016c6cbe16764cac8430710d00446a65b1
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-20 23:00:45 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    int w, h;
+    w = a + 8;
+    h = b * 3;
+    cout << w << endl;
+    cout << h << endl;
+    cout << w * h;
+    return 0;
+}
