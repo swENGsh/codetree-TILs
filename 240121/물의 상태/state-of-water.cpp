7785f75f52067ebe9344ba36b82bce53cc8d059b
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-21 21:51:36 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240121/물의 상태/state-of-water.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int temp;
+    cin >> temp;
+    if (temp < 0) {
+        cout << "ice";
+    }
+    else if (temp > 100) {
+        cout << "vapor";
+    }
+    else {
+        cout << "water";
+    }
+    return 0;
+}
