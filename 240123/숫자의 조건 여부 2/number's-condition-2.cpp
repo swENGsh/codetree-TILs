8ea87b3aa4e9b0157824502be494f6e686bcf40c
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-23 00:42:04 +0900

    Title: 숫자의 조건 여부 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240123/숫자의 조건 여부 2/number's-condition-2.cpp
+++ 240123/숫자의 조건 여부 2/number's-condition-2.cpp
@@ -5,14 +5,12 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a;
-    char ans;
     cin >> a;
     if (a == 5){
-        ans = 'A';
+        cout << 'A';
     }
     if (a / 2 == 0){
-        ans = 'B';
+        cout << 'B';
     }
-    cout << ans;
     return 0;
 }
