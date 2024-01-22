a9c73a0ba15f2c1358a6e67faf198b84c2c1b511
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-23 01:04:44 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    if (a % 3 == 0){
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+
+    if (a % 5 == 0){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+    return 0;
+}
