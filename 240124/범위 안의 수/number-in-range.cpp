8f80ab299602ac337291ed11c5b729558fbece47
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-24 23:37:31 +0900

    Title: 범위 안의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    cout << (10 <= a && a <= 20 ? "yes" : "no") << endl;
+    return 0;
+}
