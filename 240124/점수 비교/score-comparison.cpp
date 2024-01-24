28b9a709552bc4c8f190e1d4756110c52659b1d5
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-24 23:35:57 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/점수 비교/score-comparison.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, math, eng;
+    cin >> a >> b >> math >> eng;
+    cout << (a > math && b > eng) << endl;
+    return 0;
+}
