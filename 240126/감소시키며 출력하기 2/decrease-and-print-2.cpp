1de27c6d240998e72d0d9c83217e0d6eceaff004
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-26 19:39:49 +0900

    Title: 감소시키며 출력하기 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int cur = 26;
+    while(cur >= 10){
+        cout << cur << " ";
+        cur --;
+    }
+    return 0;
+}
