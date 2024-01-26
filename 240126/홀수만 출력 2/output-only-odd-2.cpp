98bb8ec8c1cc94ff85628cc146207c893e067502
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-26 19:23:41 +0900

    Title: 홀수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/홀수만 출력 2/output-only-odd-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int b, a;
+    cin >> b >> a;
+    for (b; b >=a; b-=2){
+        cout << b << " ";
+    }
+    return 0;
+}
