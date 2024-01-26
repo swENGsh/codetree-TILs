8534562b384d1bb52aa8719603b4bf29140ef94e
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-26 19:22:10 +0900

    Title: b부터 a까지 감소, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/b부터 a까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    for (b; b>=a; b--){
+        cout << b << " ";
+    }
+    return 0;
+}
