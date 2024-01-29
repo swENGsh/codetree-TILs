248d2dba44960f8d6583de19c84ced3e922c6428
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-30 01:45:31 +0900

    Title: 직각 삼각형 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i=1; i<=n; i++){
+        for (int j=0; j<(2*i)-1; j++){
+            cout << '*';
+        }
+        cout << endl;
+    }
+    return 0;
+}
