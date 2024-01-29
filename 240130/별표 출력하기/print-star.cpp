b1ab84f0894f191951a6b4c13657d52230c2e55b
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-01-30 01:55:48 +0900

    Title: 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/별표 출력하기/print-star.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i=0; i<n; i++){
+        for (int j=n; j>=n-i; j--){
+            cout << '*' << ' ';
+        }
+        cout << endl;
+    }
+    for (int i=1; i<n; i++){
+        for (int j=0; j<n-i; j++){
+            cout << '*' << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
