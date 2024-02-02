9d3112d2e798e86be6a2f4b59232da0c26d24822
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-02-02 11:26:25 +0900

    Title: 순서쌍 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240202/순서쌍 만들기/making-order-pair.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i=n; i>0; i--){
+        for (int j=n; j>0; j--){
+            cout << "(" << i << "," << j << ") ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
