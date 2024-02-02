a91d06a08fd91db2a2f720a8ac3da040520eda58
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-02-02 10:00:27 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240202/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cur, cur2;
+    cin >> n;
+    cur = 1;
+    cur2 = n-1;
+
+    for (int i=0; i<n*2; i++){
+        if (i%2==0){
+            for (int k=0; k<=cur2;k++){
+                cout << '*' << ' ';
+            }
+            cur2 --;
+        }
+        else{
+            for (int k=0; k<cur; k++){
+                cout << '*' << ' ';
+            }
+            cur ++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
