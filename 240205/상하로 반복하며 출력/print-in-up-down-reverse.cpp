dd216e411176f85672ab7efb58d51e8d1f3ddb62
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-02-05 11:17:28 +0900

    Title: 상하로 반복하며 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240205/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i=1; i<=n; i++){
+        for (int j=0; j<n; j++){
+            if (j%2==0){
+                cout << i;
+            }
+            else{
+                cout << n-i+1;
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
