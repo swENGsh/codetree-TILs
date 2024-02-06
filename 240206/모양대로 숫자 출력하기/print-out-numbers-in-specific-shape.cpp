baba28ec6da5f444b125e49b7a67d029755abe2f
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-02-06 10:48:52 +0900

    Title: 모양대로 숫자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240206/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i=n; i>0; i--){
+        for (int j=n-i; j>0; j--){
+            cout << ' ' << ' ';
+        }
+        for (int j=i; j>0; j--){
+            cout << j << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
