503965137c8cc00f2b20ef5afd33736f064da55c
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-02-06 10:37:26 +0900

    Title: 숫자 피라미드 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240206/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, tmp;
+    cin >> n;
+    tmp = 1;
+    for (int i=1; i<=n; i++){
+        for (int j=0; j<i; j++){
+            cout << tmp << ' ';
+            tmp ++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
