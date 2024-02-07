a8a55c3ae63381bd81da982bfe4656fde9afc521
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-02-07 14:09:57 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240207/배열 채우기/filling-array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10], cnt;
+    cnt = 0;
+    for (int i=0; i<10; i++){
+        cin >> arr[i];
+        if (arr[i] == 0) {
+            cnt = i-1;
+            break;
+        }
+    }
+    for (int i=cnt; i>=0; i--){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
