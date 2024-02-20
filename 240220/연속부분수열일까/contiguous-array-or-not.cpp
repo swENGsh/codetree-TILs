0ec36ba487af086de99fa7e8a8134032e7503434
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-02-20 15:29:03 +0900

    Title: 연속부분수열일까, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240220/연속부분수열일까/contiguous-array-or-not.cpp
+++ 240220/연속부분수열일까/contiguous-array-or-not.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int n1, n2;
-int* A = new int[n1];
-int* B = new int[n2];
-
-bool same(int n) {
-    for (int f = 0; f < n2; f++) {
-        if (B[f] != A[n + f]) return false;
-    }
-    return true;
-}
-
-bool is_subseq() {
-    for (int i = 0; i < n1 - n2 + 1; i++) {
-         if (same(i)) return true;
-    }
-    return false;
-}
-
 int main() {
-    // 여기에 코드를 작성해주세요.
+    int n1, n2;
     cin >> n1 >> n2;
-    for (int i = 0; i < n1; i++) cin >> A[i];
-    for (int i = 0; i < n2; i++) cin >> B[i];
-
-    if (is_subseq()) cout << "Yes";
-    else cout << "No";
+    
+    int A[n1], B[n2];
+    
+    for (int i = 0; i < n1; ++i)
+        cin >> A[i];
+    for (int i = 0; i < n2; ++i)
+        cin >> B[i];
+    
+    // 수열 B가 수열 A의 연속부분수열인지 확인
+    bool isSubsequence = false;
+    for (int i = 0; i <= n1 - n2; ++i) {
+        bool found = true;
+        for (int j = 0; j < n2; ++j) {
+            if (A[i + j] != B[j]) {
+                found = false;
+                break;
+            }
+        }
+        if (found) {
+            isSubsequence = true;
+            break;
+        }
+    }
+    
+    if (isSubsequence)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+    
     return 0;
 }
