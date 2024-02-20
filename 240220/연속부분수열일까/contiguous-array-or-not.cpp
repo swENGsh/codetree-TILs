6ee00369fec8823116df34b04c9248b84da0edd8
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-02-20 15:02:51 +0900

    Title: 연속부분수열일까, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240220/연속부분수열일까/contiguous-array-or-not.cpp
+++ 240220/연속부분수열일까/contiguous-array-or-not.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 using namespace std;
+
+int n1, n2;
+int* A = new int[n1];
+int* B = new int[n2];
+
+bool same(int n) {
+    for (int f = 0; f < n2; f++) {
+        if (B[f] != A[n + f]) return false;
+    }
+    return true;
+}
+
+bool is_subseq() {
+    for (int i = 0; i < n1 - n2 + 1; i++) {
+         if (same(i)) return true;
+    }
+    return false;
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n1, n2;
     cin >> n1 >> n2;
-    int* A = new int[n1];
-    int* B = new int[n2];
-    for (int i=0; i<n1; i++) cin >> A[i];
-    for (int i=0; i<n2; i++) cin >> B[i];
-    bool tf1 = true;
-    bool tf2 = true;
-    for (int i=0; i<n1; i++){
-        if (B[0] == A[i]){
-            for (int f=1; f<n2; f++){
-                if (B[f] != A[i+f]) {
-                    cout << "No";
-                    tf1 = false;
-                    tf2 = false;
-                    break;
-                }
-            }
-            if (tf2) {
-                cout << "Yes";
-                tf1 = false;
-                break;
-            }
-        }
-    }
-    if (tf1) cout << "No";
+    for (int i = 0; i < n1; i++) cin >> A[i];
+    for (int i = 0; i < n2; i++) cin >> B[i];
+
+    if (is_subseq()) cout << "Yes";
+    else cout << "No";
     return 0;
 }
