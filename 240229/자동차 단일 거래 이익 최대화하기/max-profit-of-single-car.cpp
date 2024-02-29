e1d733473eb398e15d589c8fa1fe6d8b1b9e25b9
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-02-29 22:56:09 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240229/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
+++ 240229/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n, max_val, min_val, idx;
+    int n, max_val, min_val, idx, tmp, ans;
     cin >> n;
-    max_val = idx = 0;
+    max_val = idx = tmp = ans = 0;
     min_val = INT_MAX;
     int* arr = new int[n];
     for (int i=0; i<n; i++) cin >> arr[i];
@@ -14,14 +14,20 @@ int main() {
         if (min_val > arr[i]){
             min_val = arr[i];
             idx = i;
+            for (int j=i; j<n; j++){
+                if (max_val < arr[j]){
+                    max_val = arr[j];
+                    tmp = max_val - min_val;
+                }
+            }
+            if (tmp > ans) {
+                ans = tmp;
+                tmp = 0;
+            }
         }
     }
-    for (int i=idx; i<n; i++){
-        if (max_val < arr[i]){
-            max_val = arr[i];
-        }
-    }
-    if (max_val - min_val > 0){
+
+    if (ans > 0){
        cout << max_val - min_val;
     }
     else {
