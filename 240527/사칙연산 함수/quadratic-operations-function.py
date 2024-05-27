3d6c6d8916d960ca156e875dfc6b27d1f8fcb98e
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-05-27 21:47:17 +0900

    Title: 사칙연산 함수, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240527/사칙연산 함수/quadratic-operations-function.py
+++ 240527/사칙연산 함수/quadratic-operations-function.py
@@ -7,8 +7,9 @@ def solve(a, o, c):
         return a//c
     if o == '*':
         return a*c
+    return False
 
 a, o, c = input().split()
 a = int(a)
 c = int(c)
-print(f'{a} {o} {c} = {solve(a, o, c)}')
+print(f'{a} {o} {c} = {solve(a, o, c)}') if solve(a, o, c) else print("False")
