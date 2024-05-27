4452c23035dcb0f44c39ba5887d54fa5966a66f9
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-05-27 22:40:31 +0900

    Title: 2021년 날짜의 유무, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240527/2021년 날짜의 유무/with-or-without-2021.py
+++ 240527/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,10 +1,15 @@
 def solve(m, d):
-    if m <= 12:
+    if m <= 7:
         if m == 2 and d <= 28:
             if m % 2 == 0 and d <= 30:
                 return True
             return True
         if m % 2 and d <= 31:
+            return True
+    elif m <= 12:
+        if m % 2 == 0 and d <= 31:
+            return True
+        if m % 2 and d <= 30:
             return True
         
     return False
