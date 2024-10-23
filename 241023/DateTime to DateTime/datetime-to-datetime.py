d464cc7728fb2dc0d6304e893b3278f0938dfab4
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-10-23 16:21:07 +0900

    Title: DateTime to DateTime, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 241023/DateTime to DateTime/datetime-to-datetime.py
+++ 241023/DateTime to DateTime/datetime-to-datetime.py
@@ -2,9 +2,17 @@ a, b, c = map(int, input().split())
 time = 0
 d = h = m = 11
 while True:
-    if a < 11 or b < 11 or c < 11:
+    if a < 11:
         print(-1)
         break
+    elif a == 11:
+        if b < 11:
+            print(-1)
+            break
+        elif b == 11:
+            if c < 11:
+                print(-1)
+                break
     if d == a and h == b and m == c:
         print(time)
         break
