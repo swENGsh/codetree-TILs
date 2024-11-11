c47cadb8e7115f327c67daaa36413d4a64fd4b68
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-11-11 14:35:01 +0900

    Title: 그 요일은, Time: 44ms, Memory: 15MB, Status: Passed - CodeTree

--- 241111/그 요일은/the-day-of-the-day.py
+++ 241111/그 요일은/the-day-of-the-day.py
@@ -4,18 +4,11 @@ days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 week = {'Mon':0, 'Tue':1, 'Wed':2, 'Thu':3, 'Fri':4, 'Sat':5, 'Sun':6}
 ans = 1
 if m1 == m2:
-    if d2 - d1 + 1 > week[A]:
-        cnt = (d2 - d1 + 1) - week[A]
-    else:
-        cnt = 0
-        ans = 0
+    cnt = (d2 - d1)
 else:
-    if ((days[m1] - d1 + 1) + d2) > week[A]:
-        cnt = ((days[m1] - d1 + 1) + d2) - week[A]
-    else:
-        cnt = 0
-        ans = 0
+    cnt = ((days[m1] - d1) + d2)
 for i in range(m1+1, m2):
     cnt += days[i]
-ans += cnt // 7
+
+ans += (cnt-week[A]) // 7
 print(ans)
