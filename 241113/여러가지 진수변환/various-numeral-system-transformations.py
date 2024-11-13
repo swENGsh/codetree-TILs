6f98f29af63bb27ec174f108c7fe06c1fd59a510
Seunghyo Ko <97648071+swENGsh@users.noreply.github.com>
2024-11-13 15:39:53 +0900

    Title: 여러가지 진수변환, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241113/여러가지 진수변환/various-numeral-system-transformations.py
@@ -0,0 +1,14 @@
+def to_binary(num, b):
+    binary = []
+    while num > 0:
+        if num == 1:
+            binary.append(num)
+            binary.reverse()
+            return binary
+        binary.append(num % b)
+        num //= b
+    return [0]
+
+n, b = map(int,input().split())
+ans = to_binary(n, b)
+print(''.join(map(str, ans)))
